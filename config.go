f2d8c6229ee189747006955ecb7805a11f2013b8
Sapo Arty <sapoarty@gmail.com>
2023-11-26 15:48:29 +0000

    add config

---
+++ config.go
@@ -0,0 +1,10 @@
+package main
+
+var botToken string
+var goldApiToken [2]string
+
+func init() {
+	botToken = "<YOUR BOT TOKEN>"
+	goldApiToken[0] = "goldapi-1-token"
+	goldApiToken[1] = "goldapi-2-token"
+}
